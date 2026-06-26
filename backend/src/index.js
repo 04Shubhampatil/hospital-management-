b94d9e29c76cc37732815333beb48b41fef96b43
04Shubhampatil <shubhammanilal2004@gmail.com>
2026-06-26 14:38:15 +0530

    cors
    Signed-off-by: 04Shubhampatil <shubhammanilal2004@gmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,15 +11,7 @@ import doctorReportRoutes from "./routes/doctorReports.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      "https://smart-hospital-crm.netlify.app",
-      "https://hospital-management-gamma-two.vercel.app",
-    "  http://localhost:5173"
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
